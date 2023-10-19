f820f013563766199d7becad4e31d40fad11749d
CristopherMoralesF <71911123+CristopherMoralesF@users.noreply.github.com>
2023-10-18 21:42:54 -0600

    Cris 18/10/2023
    Creation of MVC and API project on .NET Core.

---
+++ assetManagement/assetManagement/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using assetManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace assetManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
